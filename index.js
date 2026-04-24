27e42155df8dd2caf2b7aaba75bb54c5dfd440b2
shopreebo-coder <shopreebo@gmail.com>
2026-04-24 22:26:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+console.log("BOT VERSION TELEGRAM ACTIVE 🚀");
+
 const TOKEN = "8659223122:AAFvSZw6wnAPOuEUZMhuufw0Xu4QzZ8BEeOo";
 const CHAT_ID = "7209483091";
 
 const SYMBOL = "BTCUSDT";
 
-console.log("Trading bot start ✅");
-
 async function sendTelegram(message) {
   try {
-    const res = await fetch(
+    await fetch(
       `https://api.telegram.org/bot${TOKEN}/sendMessage`,
       {
         method: "POST",
@@ -20,56 +20,15 @@ async function sendTelegram(message) {
         })
       }
     );
-
-    console.log("Telegram status:", res.status);
   } catch (err) {
     console.log("Telegram error:", err.message);
   }
 }
 
 async function getPrice() {
-  try {
-    const res = await fetch(
-      `https://api.binance.com/api/v3/ticker/price?symbol=${SYMBOL}`
-    );
+  const res = await fetch(
+    `https://api.binance.com/api/v3/ticker/price?symbol=${SYMBOL}`
+  );
 
-    const data = await res.json();
-
-    return parseFloat(data.price);
-  } catch (err) {
-    console.log("Binance error:", err.message);
-  }
-}
-
-let lastPrice = null;
-
-setInterval(async () => {
-
-  const price = await getPrice();
-
-  if (!price) return;
-
-  console.log("Cena BTC:", price);
-
-  await sendTelegram("Cena BTC: " + price);
-
-  if (lastPrice) {
-
-    if (price > lastPrice) {
-      await sendTelegram("📈 BUY signal");
-    }
-
-    if (price < lastPrice) {
-      await sendTelegram("📉 SELL signal");
-    }
-
-  }
-
-  lastPrice = price;
-
-}, 60000);
-
-
-setInterval(() => {
-  console.log("heartbeat ❤️ bot alive");
-}, 30000);
+  const data = await res.json();
+  return
